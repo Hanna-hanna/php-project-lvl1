c669bf9bbf00834f879e3f011384a7736146f9a8
Hanna Herasimava <annlisandias@gmail.com>
2021-07-02 14:29:17 +0200

    Add PHP_CodeSniffer & make lint

--- src/Cli.php
+++ src/Cli.php
@@ -1,9 +1,13 @@
-<?php	
-	namespace Brain\Games\Cli;
-	use function cli\line;
-	use function cli\prompt;
-	function greeting() {
-		line('Welcome to the Brain Game!');
-		$name = prompt('May I have your name?');
-		line("Hello,%s!", $name);
-	}
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello,%s!", $name);
+}
